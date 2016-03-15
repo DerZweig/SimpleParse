1d3a2912d0d1038051b44e96246e823657d35255
yunharla <broodtech@live.de>
2016-03-15 09:40:56 +0100

    -Simple Parse Library

---
+++ Parser.fs
@@ -0,0 +1,71 @@
+﻿module Parser
+open Tokenizer
+
+type ParserResult<'t> = 
+    | Result of 't
+    | Error of int * string list
+type ParserFunc<'t> = Text -> ParserResult<'t> * Text
+type Parser<'t> = { Names : string list ; Eval : ParserFunc<'t> }
+type ParserBuilder(name) =
+    member self.Return(x) = {Names = [name]; Eval = fun cs -> Result(x),cs}
+    member self.ReturnFrom(x) = {Names = [name]; Eval = x.Eval}
+    member self.Bind(m : Tokenizer,f : (int*string) -> Parser<'a>) = {
+        Names = [name]
+        Eval = fun cs ->
+            match m.RunWithText cs with
+            | Some(r),rs -> 
+                match (f (r.Line,new string[|for c in r.Value -> c|])).Eval rs with
+                | Result(x),xs -> Result(x),xs
+                | Error(el,es),_ -> Error(el,m.Names @ es),cs
+            | _ -> Error(cs.Line,m.Names),cs
+    }
+    member self.Bind(p : Parser<'a>,f : 'a -> Parser<'b>) = {
+        Names = [name]
+        Eval = fun cs -> 
+            match p.Eval cs with
+            | Result(r),rs ->
+                match (f r).Eval rs with
+                | Result(x),xs -> Result(x),xs
+                | Error(el,es),_ -> Error(el,p.Names @ es),cs
+            | Error(el,es),_ -> Error(el,es),cs
+    }
+    member self.Zero() = {
+        Names = [name]
+        Eval = fun cs -> Error(cs.Line,[name]),cs
+    }
+
+let parser(name) = ParserBuilder(name)
+type Parser with
+    static member (<|>) (p: Parser<'t>,q : Parser<'t>) = {
+        Names = (p.Names) @ (q.Names)
+        Eval = fun cs -> match p.Eval cs with
+                         | Result(r),rs -> Result(r),rs
+                         | _,_ -> q.Eval cs
+    }
+    
+    member self.Repeat = 
+        let rec many1 (p:Parser<_>) : Parser<_> = (parser("") {
+            let! r = p
+            let! rs = many p
+            return r::rs
+        })
+        and many p : Parser<_> = (many1 p) <|> ((parser("") { return [] }))
+        {(many1 self) with Names = self.Names}
+    member self.SeparatedBy (x:Tokenizer) = 
+        let rec many1 (p:Parser<_>) : Parser<_> = (parser("") {
+            let! r = p
+            let! _ = x
+            let! rs = many p
+            return r::rs
+        })
+        and many p : Parser<_> = (many1 p) <|> ((parser("") { return [] }))
+        let p = {(many self) with Names = self.Names}
+        let p2 = parser("") {
+            let! xs = p
+            let! x = self
+            return xs @ [x]
+        }
+        {p2 with Names = self.Names}
+    member self.RunWithText s = self.Eval s
+    member self.RunWithString s = self.RunWithText(Text.FromString s)
+    
---
+++ Tokenizer.fs
@@ -0,0 +1,72 @@
+﻿module Tokenizer
+
+type Text = { Line : int; Chars: char list }
+type Token = { Name: string; Line : int; Value : char list }
+type TokenFunc = Text -> Token option * Text
+type Tokenizer = { Names : string list; Eval : TokenFunc }
+type Text with 
+    member self.IsEmpty = self.Chars.IsEmpty
+    static member FromString s = {Line = 1; Chars = [for c in s -> c]}
+type Token with
+    static member JustValue s = {Name = ""; Line = 0; Value = s}
+let private move1 (cs:Text) = function
+    | '\n' -> {cs with Line = cs.Line + 1}
+    | _ -> cs
+type TokenBuilder(name) = 
+    member self.ReturnFrom(x: TokenFunc) = {
+        Names = [name]
+        Eval = fun cs -> match x cs with
+                         | Some(r),rs -> Some({r with Line = cs.Line; Name = name}),rs
+                         | _ -> None,cs}
+    member self.ReturnFrom(p : char -> bool) = 
+        self.ReturnFrom(fun cs ->
+            match cs.Chars with
+            | r::rs when p r -> Some(Token.JustValue([r])),move1 {cs with Chars = rs} r
+            | _ -> None,cs)
+    member self.Bind(m : Tokenizer,f) = self.ReturnFrom(fun cs -> 
+        match m.Eval cs with
+        | Some(r),rs -> 
+            let t2 = f(new string [|for c in r.Value -> c|])
+            match t2.Eval rs with
+            | Some(r2),r2s -> Some(Token.JustValue(r2.Value)),r2s
+            | _ -> None,cs
+        | _ -> None,cs)
+    member self.Zero() = self.ReturnFrom(fun cs -> None,cs)
+    member self.Return(c : char list) = self.ReturnFrom(fun cs -> Some(Token.JustValue(c)),cs)
+    member self.Return(s : char seq) = self.Return([for c in s -> c])
+    member self.Bind(m : char->bool,f) = self.Bind(self.ReturnFrom m,f)
+    member self.ReturnFrom(txt: char seq) =
+        let rec text = function
+            | [] -> self.Return []
+            | x::xs -> TokenBuilder(name) {
+                let! _ = (=) x
+                let! _ = text xs
+                return x::xs}
+        text [for c in txt -> c]
+    member self.Bind(m : char seq,f) = self.Bind(self.ReturnFrom m,f)
+let token(name) = TokenBuilder(name)
+type Tokenizer with
+    static member (<|>) (p: Tokenizer,q : Tokenizer) = {
+        Names = (p.Names) @ (q.Names)
+        Eval = fun cs -> match p.Eval cs with
+                         | None,_ -> q.Eval cs
+                         | r,rs -> r,rs }
+    member self.OrNothing = self <|> ({token("") { return []} with Names = self.Names})
+    member self.Repeat = 
+        let rec many1 (p:Tokenizer) : Tokenizer = (token("") {
+            let! r = p
+            let! rs = many p
+            return r+rs
+        })
+        and many p : Tokenizer = (many1 p) <|> ((token("") { return [] }))
+        {(many1 self) with Names = self.Names}
+    member self.RepeatUntil (x:Tokenizer) = 
+        let rec until (p:Tokenizer) : Tokenizer = p <|>  (token("") {
+            let! s = self
+            let! r = until p
+            return s + r
+        })
+        {(until x) with Names = self.Names}
+        
+    member self.RunWithText s = self.Eval s
+    member self.RunWithString s = self.RunWithText (Text.FromString s)
